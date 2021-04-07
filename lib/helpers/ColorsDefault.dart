c9994c4bfed3aecb88007d964a8d258f74e023c1
freddymtd <freddy-zenteno@hotmail.com>
2021-04-07 20:57:47 -0300

    Projeto Final

---
+++ lib/helpers/ColorsDefault.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorDefault {
+  static Color primary = Color.fromRGBO(52, 43, 37, 1);
+  static Color gray = Color.fromRGBO(137, 137, 137, 1);
+  static Color secoundry = Color.fromRGBO(198, 116, 27, 1);
+  static Color secundryLight = Color.fromRGBO(52, 43, 37, 1);
+}
